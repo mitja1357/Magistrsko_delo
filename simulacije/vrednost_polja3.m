d4f2a7724fe253a20f3c6df16b799c20dc938218
Studenti <studenti@example.net>
2017-06-08 09:58:32 +0200

    nastavil pozicije slik

--- simulacije/vrednost_polja3.m
+++ simulacije/vrednost_polja3.m
@@ -1,8 +1,9 @@
-clear all
+clear all;
+close all;
 %definiraj ekscentricnost
-xs=0+0*(rand()-0.5);
+xs=0.0+0*(rand()-0.5);
 ys=0+0*(rand()-0.5);
-xd=0.1+0*(rand()-0.5);
+xd=0.10+0*(rand()-0.5);
 yd=0+0*(rand()-0.5);
 % definiraj zacetni poziciji sond
 zac_x=[1;0];
@@ -10,10 +11,22 @@ zac_y=[0;1];
 % definiraj kote
 theta=linspace(-pi,pi,1000);
 % priprava slike
-figure(2)
+
+
+ime_slik=strcat('xs=',num2str(xs),', ','yd=',num2str(ys),', ','xd=',num2str(xd),', ','yd=',num2str(yd));
+
+
+fig_BxBy=figure('Name',strcat('Bx,By, ',ime_slik),'numbertitle','off','Position',[0   550   560   420]);
 clf
 hold on
 
+fig_fft_protokol=figure('Name',strcat('fft protokola, ',ime_slik),'numbertitle','off','Position',[560   550   560   420]);
+clf
+hold on
+
+fig_protokol=figure('Name',strcat('Protokol, ',ime_slik),'numbertitle','off','Position',[1120   550   560   420]);
+clf
+hold on
 
 Bx=zeros(size(theta));
 By=zeros(size(theta));
@@ -34,23 +47,24 @@ By(stevec)=hally(1);
 
 stevec=stevec+1;
 end
-
+figure(fig_BxBy)
+plot(theta,Bx,'-b')
+plot(theta,By,'-r')
+grid on
+legend('B_x','B_y')
 
 kot_merjeni=atan2(Bx,By);
 
 poprava_kota;
 
-figure(3)
 
+figure(fig_protokol)
 plot(kot_merjeni-theta,'-r')
-hold off
-figure(2)
-
-plot(theta,Bx,'-b',theta,By,'-r')
 grid on
-legend('B_x','B_y')
+
 
 fftprotokol=fft(kot_merjeni-theta);
 
-figure(1)
-plot(abs(fftprotokol))
+figure(fig_fft_protokol)
+plot(0:9,abs(fftprotokol(1:10)))
+grid on
