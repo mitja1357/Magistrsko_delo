f602edc0859069f65a19fed282863ac008af394a
RLS\alic <mitja1357@gmail.com>
2018-11-07 13:00:02 +0100

    popravljal barve slike

--- simulacije/Slika_polje_in_linearizacija.m
+++ simulacije/Slika_polje_in_linearizacija.m
@@ -1,8 +1,8 @@
 close all
-load('X_tmp.mat');
-load('Y_tmp.mat');
-load('Zmesh.mat');
-ravnina = 0;
+load('razno\X_tmp_stari.mat');
+load('razno\Y_tmp_stari.mat');
+load('razno\Zmesh_stari.mat');
+ravnina = 1;
 gif = 0;
 n= 30;
 X = X_tmp(1+n:(1001-n));
@@ -45,7 +45,11 @@ t =  linspace(-130,-50,1000000);
 %%
 h = figure('Color',[1 1 1],'Position',[50,100,800,4000]);
 barva = linspace(0,1,128);
-cmap = [0.9*[barva(:),barva(:),barva(:)];[zeros(128,1),zeros(128,1),zeros(128,1)+barva(:)]];
+red= 	[zeros(32,1);		zeros(32,1);		linspace(0,1,32)';	ones(32,1)];
+green= 	[linspace(0,1,32)';	ones(32,1);		ones(32,1);		linspace(1,0,32)'];
+blue= 	[ones(32,1);		linspace(1,0,32)';	zeros(32,1);		zeros(32,1)];
+ 
+cmap = [[red, green, blue];[zeros(128,1),zeros(128,1),zeros(128,1)+barva(:)]];
 colormap(cmap)
 filename = 'polje_brez_ravnino.gif';
 clf
@@ -53,22 +57,23 @@ clf
 axes1 = axes('Visible','off','Parent',h);
 view(axes1,[80 8]);
 hold(axes1,'all');
-
+if ravnina
+    filename = 'polje_z_ravnino.gif';
+     barvapolje_rav1 = 160+127*(Zravn-min(Zravn(:)))/(max(Zravn(:))-min(Zravn(:)));
+    surf(Y,X,Zravn,'Parent',axes1,'cdata',...
+     round(barvapolje_rav1),'cdatamapping','direct');
+end
 barvapolje = 1+127*(Zn-min(Zn(:)))/(max(Zn(:))-min(Zn(:)));
 
 % Create polje
-surf(Xn,Yn,Zn,'Parent',axes1,'cdata',...
-    round(barvapolje),'cdatamapping','direct');
+surf(Xn,Yn,Zn,'Parent',axes1,'cdatamode',...
+    'auto','cdatamapping','scaled');
 
 surf(Xc,Yc,4*Zc-50-4,'FaceColor',...
      [0.5686274766922 0.521568655967712 0.458823531866074],'EdgeColor','none');
 % Create ravnina
-if ravnina
-    filename = 'polje_z_ravnino.gif';
-    barvapolje_rav = 160+127*(Zravn-min(Zravn(:)))/(max(Zravn(:))-min(Zravn(:)));
-    surf(Y,X,Zravn,'Parent',axes1,'cdata',...
-    round(barvapolje_rav),'cdatamapping','direct');
-end
+%%
+
 %%
 theta = linspace(0, 2*pi, 100);
 
